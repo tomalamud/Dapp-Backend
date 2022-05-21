a01564e66b0f8989db74b5ba31cfe40f93bf95e2
tomaselizondo <tomaslelizondo@gmail.com>
2022-05-20 22:57:33 -0300

    solidity

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+const deploy = async () => {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying from account: ", deployer.address)
+
+    const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+    const deployed = await PlatziPunks.deploy();
+
+    console.log("deployed contract: ", deployed.address);
+}
+
+deploy()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.log(err);
+        process.exit(1);
+    })
